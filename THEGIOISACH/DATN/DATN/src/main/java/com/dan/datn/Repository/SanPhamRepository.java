852a778a12d929ddfe74910fc1f40eeb3684d639
anhkiet <125857394+kkkkkkkkikikkkki@users.noreply.github.com>
2024-11-03 18:21:21 +0700

    Delete THEGIOISACH directory nhánh dan

--- THEGIOISACH/DATN/DATN/src/main/java/com/dan/datn/Repository/SanPhamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dan.datn.Repository;
-
-import com.dan.datn.Entity.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SanPhamRepository extends JpaRepository<SanPham, Long> {
-}
