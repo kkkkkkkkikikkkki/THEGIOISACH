af4b75bb90e5bec0df981a772e2ada4d3f906270
LeNgocKhanhThu <thukhanhngocle240203@gmail.com>
2024-11-05 16:40:49 +0700

    api san pham chi tiet

--- THEGIOISACH/DATN/DATN/src/main/java/com/dan/datn/Controller/SanPhamController.java
+++ THEGIOISACH/DATN/DATN/src/main/java/com/dan/datn/Controller/SanPhamController.java
@@ -26,29 +26,29 @@ public class    SanPhamController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-//
-//    @PostMapping
-//    public ResponseEntity<SanPham> createSanPham(@RequestBody SanPham sanPham) {
-//        SanPham createdSanPham = sanPhamService.saveSanPham(sanPham);
-//        return ResponseEntity.status(201).body(createdSanPham);
-//    }
 
-//    @PutMapping("/{id}")
-//    public ResponseEntity<SanPham> updateSanPham(@PathVariable String id, @RequestBody SanPham sanPham) {
-//        return sanPhamService.getSanPhamById(id)
-//                .map(existingSanPham -> {
-//                    sanPham.setIdSanPham(existingSanPham.getIdSanPham()); // Set ID to ensure we're updating the correct product
-//                    return ResponseEntity.ok(sanPhamService.saveSanPham(sanPham));
-//                })
-//                .orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteSanPham(@PathVariable String id) {
-//        if (sanPhamService.getSanPhamById(id).isPresent()) {
-//            sanPhamService.deleteSanPham(id);
-//            return ResponseEntity.noContent().build();
-//        }
-//        return ResponseEntity.notFound().build();
-//    }
+    @PostMapping
+    public ResponseEntity<SanPham> createSanPham(@RequestBody SanPham sanPham) {
+        SanPham createdSanPham = sanPhamService.saveSanPham(sanPham);
+        return ResponseEntity.status(201).body(createdSanPham);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SanPham> updateSanPham(@PathVariable String id, @RequestBody SanPham sanPham) {
+        return sanPhamService.getSanPhamById(id)
+                .map(existingSanPham -> {
+                    sanPham.setIdSanPham(existingSanPham.getIdSanPham()); // Set ID to ensure we're updating the correct product
+                    return ResponseEntity.ok(sanPhamService.saveSanPham(sanPham));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSanPham(@PathVariable String id) {
+        if (sanPhamService.getSanPhamById(id).isPresent()) {
+            sanPhamService.deleteSanPham(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
