47b1ca2b81913a483821cfbdccea420077f3801b
anhkiet <125857394+kkkkkkkkikikkkki@users.noreply.github.com>
2024-11-07 23:09:21 +0700

    Delete THEGIOISACH directory

--- THEGIOISACH/DATN/DATN/src/main/java/com/dan/datn/Repository/HoaDonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dan.datn.Repository;
-
-import com.dan.datn.Entity.HoaDon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HoaDonRepository extends JpaRepository<HoaDon, Integer> {
-}
