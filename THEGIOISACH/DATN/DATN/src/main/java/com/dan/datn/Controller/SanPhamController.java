4e47ff2f2f05d390e93d8f0d3f53c40a4a22f672
HuynhDa <huynhcaodan123@gmail.com>
2024-11-11 14:37:27 +0700

    Done tim kiem

--- THEGIOISACH/DATN/DATN/src/main/java/com/dan/datn/Controller/SanPhamController.java
+++ THEGIOISACH/DATN/DATN/src/main/java/com/dan/datn/Controller/SanPhamController.java
@@ -3,52 +3,41 @@
 //import com.dan.datn.Entity.SanPham;
 //import com.dan.datn.Service.SanPhamService;
 //import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 //
 //import java.util.List;
 //
-//@RestController
-//@RequestMapping("/api/sanpham")
+//@Controller
 //public class SanPhamController {
 //
 //    @Autowired
 //    private SanPhamService sanPhamService;
 //
-//    @GetMapping
-//    public List<SanPham> getAllSanPhams() {
-//        return sanPhamService.getAllSanPhams();
-//    }
+//    @GetMapping("/search")
+//    public String searchSanPham(
+//            @RequestParam(required = false) String tenSach,
+//            @RequestParam(required = false) String theLoai,
+//            Model model) {
 //
-//    @GetMapping("/{id}")
-//    public ResponseEntity<SanPham> getSanPhamById(@PathVariable String id) {
-//        return sanPhamService.getSanPhamById(id)
-//                .map(ResponseEntity::ok)
-//                .orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<SanPham> createSanPham(@RequestBody SanPham sanPham) {
-//        SanPham createdSanPham = sanPhamService.saveSanPham(sanPham);
-//        return ResponseEntity.status(201).body(createdSanPham);
-//    }
-//
-////    @PutMapping("/{id}")
-////    public ResponseEntity<SanPham> updateSanPham(@PathVariable String id, @RequestBody SanPham sanPham) {
-////        return sanPhamService.getSanPhamById(id)
-////                .map(existingSanPham -> {
-////                    sanPham.setIdSanPham(existingSanPham.getIdSanPham()); // Set ID to ensure we're updating the correct product
-////                    return ResponseEntity.ok(sanPhamService.saveSanPham(sanPham));
-////                })
-////                .orElse(ResponseEntity.notFound().build());
-////    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteSanPham(@PathVariable String id) {
-//        if (sanPhamService.getSanPhamById(id).isPresent()) {
-//            sanPhamService.deleteSanPham(id);
-//            return ResponseEntity.noContent().build();
+//        List<SanPham> sanPhams ;
+//        if (tenSach != null && theLoai != null) {
+//            sanPhams = sanPhamService.searchByTenSachAndTheLoai(tenSach, theLoai);
+//        } else if (tenSach != null) {
+//            sanPhams = sanPhamService.searchByTenSach(tenSach);
+//        } else if (theLoai != null) {
+//            sanPhams = sanPhamService.searchByTheLoai(theLoai);
+//        }else {
+//            sanPhams = List.of();
 //        }
-//        return ResponseEntity.notFound().build();
+//
+//        model.addAttribute("sanPhams", sanPhams);
+//        model.addAttribute("tenSach", tenSach);
+//        model.addAttribute("theLoai", theLoai);
+//        return "index/sanPhamSauTimKiem";
 //    }
 //}
+//
+//
